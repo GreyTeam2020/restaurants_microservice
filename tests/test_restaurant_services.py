7f4888ec7c569242ba87019518499752db9327e5
Renato <Eschini>
2020-11-15 22:00:26 +0100

    refactoring dbsession

--- tests/test_restaurant_services.py
+++ tests/test_restaurant_services.py
@@ -2,10 +2,10 @@ from services import RestaurantService
 
 
 class TestRestaurantsServices:
-    def test_all_restaurant(self, db):
+    def test_all_restaurant(self):
         """
         test about the services restaurant to test the result of all restaurants
         :return:
         """
-        all_restaurants = RestaurantService.get_all_restaurants(db)
+        all_restaurants = RestaurantService.get_all_restaurants()
         assert len(all_restaurants) == 2
