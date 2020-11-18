ea63628f43153227d9a5c5f3c3ff3cc2fca8b67c
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2020-11-18 15:33:40 +0100

    Support celery with background task
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM python:3.7.0-stretch
+LABEL mantainer="Vincenzo Palazzo v.palazzo1@studenti.unipi.it"
 ADD ./ /code
 WORKDIR code
 ENV FLASK_APP=app.py
--- __init__.py
+++ __init__.py
@@ -1,2 +1,3 @@
 from .database import Restaurant, init_db
 from .app import db_session
+from .background import celery_app, calculate_rating_about_restaurant
